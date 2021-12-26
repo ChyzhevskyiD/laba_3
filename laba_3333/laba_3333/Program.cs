07707086d7f115f625cad89542f85d8482208b07
Чижевський Дмитрій <95354969+ChyzhevskyiD@users.noreply.github.com>
2021-12-26 23:03:15 +0200

    laba_3
    laba_3

---
+++ laba_3333/laba_3333/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new StudentProfessorTest("Jim");
+            test.Main();
+        }
+    }
+    class Person
+    {
+        public string name { get; set; }
+        int age;
+        public int SetAge
+        {
+            set { age = value; }
+        }
+        public int Greet
+        {
+            get { return age; }
+        }
+    }
+    class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm studying");
+        }
+        public void ShowAge(int x)
+        {
+            Console.WriteLine($"My age is: " + x + " years old");
+        }
+    }
+    class Professor : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining");
+        }
+    }
+    class StudentProfessorTest
+    {
+        string name { get; }
+        public StudentProfessorTest(string name_)
+        {
+            name = name_;
+        }
+        public void Main()
+        {
+            Console.WriteLine("Good morning said " + name);
+            var student = new Student();
+            student.SetAge = 19;
+            student.name = "Alex";
+            Console.WriteLine("Hello, my name is " + student.name + ", i'm a student");
+            student.ShowAge(student.Greet);
+            var teacher = new Professor();
+            teacher.SetAge = 40;
+            teacher.name = "Bob";
+            Console.WriteLine("Good morning, i'm " + teacher.name + ", i'm " +
+           teacher.Greet + " years old");
+            teacher.Explain();
+        }
+    }
+}
